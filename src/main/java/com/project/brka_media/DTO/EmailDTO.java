62627d2fe3bb8c8a80ab50583a0fed84e68f4ba1
milanche895 <milanstojanovic895@gmail.com>
2025-04-02 22:00:24 +0200

    make email service api

--- src/main/java/com/project/brka_media/DTO/EmailDTO.java
+++ src/main/java/com/project/brka_media/DTO/EmailDTO.java
@@ -4,8 +4,25 @@ public class EmailDTO {
 
     private String name;
     private String email;
-    private String phone;
     private String message;
+    private String subject;
+    private String firstRegistration;
+    private String brand;
+    private String model;
+    private String mileage;
+    private String zipCode;
+    private String phone;
+    private String firstName;
+    private String birthday;
+    private String country;
+    private String residence;
+    private String typeCertificate;
+    private String stateNumber;
+    private String marketPlace;
+    private String km;
+    private String lastInsurance;
+    private String desireCoverage;
+    private String typeMessage;
 
     public EmailDTO(){
 
@@ -41,5 +58,141 @@ public class EmailDTO {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public String getTypeMessage() {
+        return typeMessage;
+    }
+
+    public void setTypeMessage(String typeMessage) {
+        this.typeMessage = typeMessage;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getFirstRegistration() {
+        return firstRegistration;
+    }
+
+    public void setFirstRegistration(String firstRegistration) {
+        this.firstRegistration = firstRegistration;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(String mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getResidence() {
+        return residence;
+    }
+
+    public void setResidence(String residence) {
+        this.residence = residence;
+    }
+
+    public String getTypeCertificate() {
+        return typeCertificate;
+    }
+
+    public void setTypeCertificate(String typeCertificate) {
+        this.typeCertificate = typeCertificate;
+    }
+
+    public String getStateNumber() {
+        return stateNumber;
+    }
+
+    public void setStateNumber(String stateNumber) {
+        this.stateNumber = stateNumber;
+    }
+
+    public String getMarketPlace() {
+        return marketPlace;
+    }
+
+    public void setMarketPlace(String marketPlace) {
+        this.marketPlace = marketPlace;
+    }
+
+    public String getKm() {
+        return km;
+    }
+
+    public void setKm(String km) {
+        this.km = km;
+    }
+
+    public String getLastInsurance() {
+        return lastInsurance;
+    }
+
+    public void setLastInsurance(String lastInsurance) {
+        this.lastInsurance = lastInsurance;
+    }
+
+    public String getDesireCoverage() {
+        return desireCoverage;
+    }
+
+    public void setDesireCoverage(String desireCoverage) {
+        this.desireCoverage = desireCoverage;
     }
 }
--- src/main/java/com/project/brka_media/service/implement/EmailServiceImplement.java
+++ src/main/java/com/project/brka_media/service/implement/EmailServiceImplement.java
@@ -7,7 +7,9 @@ import com.postmarkapp.postmark.client.data.model.templates.TemplatedMessage;
 import com.postmarkapp.postmark.client.exception.PostmarkException;
 import com.project.brka_media.DTO.EmailDTO;
 import com.project.brka_media.service.EmailService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -30,14 +32,40 @@ public class EmailServiceImplement implements EmailService {
             // Kreiranje modela sa podacima za templejt
             Map<String, Object> templateModel = new HashMap<>();
             templateModel.put("name", emailDTO.getName());
-            templateModel.put("message", emailDTO.getMessage());
-            templateModel.put("phone", emailDTO.getPhone());
             templateModel.put("email", emailDTO.getEmail());
-
+            templateModel.put("message", emailDTO.getMessage());
+            templateModel.put("subject", emailDTO.getSubject());
+            templateModel.put("firstRegistration", emailDTO.getFirstRegistration());
+            templateModel.put("brand", emailDTO.getBrand());
+            templateModel.put("model", emailDTO.getModel());
+            templateModel.put("mileage", emailDTO.getMileage());
+            templateModel.put("zipCode", emailDTO.getZipCode());
+            templateModel.put("phone", emailDTO.getPhone());
+            templateModel.put("firstName", emailDTO.getFirstName());
+            templateModel.put("birthday", emailDTO.getBirthday());
+            templateModel.put("country", emailDTO.getCountry());
+            templateModel.put("residence", emailDTO.getResidence());
+            templateModel.put("typeCertificate", emailDTO.getTypeCertificate());
+            templateModel.put("stateNumber", emailDTO.getStateNumber());
+            templateModel.put("marketPlace", emailDTO.getMarketPlace());
+            templateModel.put("km", emailDTO.getKm());
+            templateModel.put("lastInsurance", emailDTO.getLastInsurance());
+            templateModel.put("desireCoverage", emailDTO.getDesireCoverage());
+            templateModel.put("email", emailDTO.getEmail());
+            System.out.println(emailDTO.getTypeMessage());
             ObjectMapper objectMapper = new ObjectMapper();
             String templateModelJson = objectMapper.writeValueAsString(templateModel);
-
-            TemplatedMessage message = new TemplatedMessage("it@buta-solution.com", "milanstojanovic895@gmail.com", 39582275);
+            Integer templateId = null;
+            if (emailDTO.getTypeMessage().equals("contact")){
+                templateId = 39582275;
+            } else if (emailDTO.getTypeMessage().equals("insurance")){
+                templateId = 39585451;
+            } else if (emailDTO.getTypeMessage().equals("sell")){
+                templateId = 39585486;
+            } else {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Please insert typeMessage: contact, sell or insurance!");
+            }
+            TemplatedMessage message = new TemplatedMessage("it@buta-solution.com", "milanstojanovic895@gmail.com", templateId);
             message.setTemplateModel(templateModel);  // OVDE setuješ mapu, ne JSON string
             message.setMessageStream("outbound");
 
